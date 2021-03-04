b4df337355d8e3b7dcdc53fa192fcc691f687089
Ryan Grey <rgrey@scottlogic.co.uk>
2021-03-04 11:43:09 +0000

    Add stub multiplatform Xcode project

---
+++ Shared/SwiftUIMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMultiplatformApp.swift
+//  Shared
+//
+//  Created by Ryan Grey on 04/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
