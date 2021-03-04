7f35e45beaf033a3761dfae05b6cdfcea94cb8d0
Ryan Grey <rgrey@scottlogic.co.uk>
2021-03-04 12:02:34 +0000

    Add tvOS target

---
+++ SwiftUIMultiPlatform (tvOS)/SwiftUIMultiPlatform__tvOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMultiPlatform__tvOS_App.swift
+//  SwiftUIMultiPlatform (tvOS)
+//
+//  Created by Ryan Grey on 04/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMultiPlatform__tvOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
